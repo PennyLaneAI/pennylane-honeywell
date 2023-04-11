cc192cb7d087d5117c07bd35714f7bb541a54d9f
Mudit Pandey <mudit.pandey@xanadu.ai>
2023-04-11 09:32:55 -0400

    Pinned Pennylane master to setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open("pennylane_honeywell/_version.py") as f:
 # Avoid pinning, and use minimum version numbers
 # only where required.
 requirements = [
-    "pennylane",
+    "pennylane @ git+https://github.com/PennyLaneAI/pennylane.git",
     "requests",
     "pyjwt",
     "toml",
